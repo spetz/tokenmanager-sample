262351210c4144948340cf130cfda69a5770f27b
Piotr Gankiewicz <spetz90@gmail.com>
2018-04-24 19:18:10 +0200

    moved to src

--- src/TokenManager.Api/Startup.cs
+++ src/TokenManager.Api/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using TokenManager.Api.Models;
 using TokenManager.Api.Services;
